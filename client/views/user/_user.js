3e25fff03f5189f5c53b7074f001b64b92d748fd
Adrienbecker <adrien.becker@gmail.com>
2014-09-04 01:08:47 +0200

    alpha : padre stat align right

--- client/views/user/_user.js
+++ client/views/user/_user.js
@@ -3,12 +3,12 @@ Template._user_detail.helpers({
 	pmdr : function () {
 
 		var pmdr = {};
-		pmdr.total = Pomodoro.find({ user_id : this._id }).fetch().length;
-		pmdr.current = Pomodoro.find({ user_id : this._id, complete : false, cancel : false }).fetch().length;
-		pmdr.complete = Pomodoro.find({ user_id : this._id, complete : true }).fetch().length;
-		pmdr.uncomplete = Pomodoro.find({ user_id : this._id, cancel : true }).fetch().length;
-		pmdr.percent_complete = pmdr.complete * 100 / ( pmdr.total - pmdr.current );
-		pmdr.percent_uncomplete = pmdr.uncomplete * 100 / ( pmdr.total - pmdr.current );
+		pmdr.current = parseInt( Pomodoro.find({ user_id : this._id, complete : false, cancel : false }).fetch().length );
+		pmdr.total = parseInt( Pomodoro.find({ user_id : this._id }).fetch().length - pmdr.current );
+		pmdr.complete = parseInt( Pomodoro.find({ user_id : this._id, complete : true }).fetch().length );
+		pmdr.uncomplete = parseInt( Pomodoro.find({ user_id : this._id, cancel : true }).fetch().length );
+		pmdr.percent_complete = parseInt( pmdr.complete * 100 / ( pmdr.total - pmdr.current ) );
+		pmdr.percent_uncomplete = parseInt( pmdr.uncomplete * 100 / pmdr.total );
 
 		return pmdr;
 	},
